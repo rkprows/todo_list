90b343af32ff191585e5ff4f355310d59898b330
Rachel Prows <rachel.prows@icloud.com>
2016-05-24 17:18:03 -0600

    add, edit, delete tasks functional

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,48 @@
+class ItemsController < ApplicationController
+  def index
+  	@items = Item.all
+  end
+
+  def new
+  	@item = Item.new
+  end
+
+  def edit
+  	@item = Item.find(params[:id])
+  end
+
+  def show
+  	@item = Item.find(params[:id])
+  end
+
+  def create
+  	@item = Item.new(item_params)
+
+  	if @item.save
+  		redirect_to items_path
+  	else
+  		render :new
+  	end
+  end
+
+  	def update
+		@item = Item.find(params[:id])
+		if @item.update(item_params)
+			redirect_to items_path
+		else
+			render :edit
+		end
+	end
+
+  def destroy
+  	Item.find(params[:id]).destroy
+  	redirect_to items_path
+  end
+
+private
+
+def item_params
+	params.require(:item).permit(:name, :description)
+end
+
+end
